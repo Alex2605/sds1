e31a002e6032a938f2004d7b276d008a7c343a21
Alexander Santos <alexander.santos.2605@gmail.com>
2020-09-20 17:02:48 -0300

    Configuring front-web for production

--- front-mobile/src/pages/CreateRecord/index.tsx
+++ front-mobile/src/pages/CreateRecord/index.tsx
@@ -14,7 +14,8 @@ const placeholder = {
     value: null
 }
 
-const BASE_URL = 'http://192.168.1.76:8080';
+/*const BASE_URL = 'http://192.168.1.76:8080'; */
+const BASE_URL = 'https://sds1-alexsantos.herokuapp.com';
 
 const mapSelectValues = (games: Game[]) => {
     return games.map(game => ({
